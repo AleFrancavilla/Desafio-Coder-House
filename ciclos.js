df4dba73fd9a5ed054ed1aea6ac86886fcb09cd3
alefranca <a.francavillapy@gmail.com>
2022-07-22 13:52:44 -0300

    For While desafio Ciclos

--- ciclos.js
+++ ciclos.js
@@ -1,6 +1,19 @@
-
-let limite= parseInt(prompt("Ingrese numero:"))
+/*
+let limite= parseInt(prompt("Ingrese un numero:"))
 for(let i= 0; i<limite; i++){
-console.log("iteracion Numero", i )
+console.log("numeritos", i )
 
+}*/
+
+//debugger
+
+while(login === false) {
+    usuario = prompt("Ingresa tu usuario:")
+    password = prompt("Ingresa tu clave:")
+    if (usuario === "Alejandra Francavilla" && password === "Aprobada") {
+        console.log("Bienvenido querido usuario", usuario)
+        login = true
+    } else {
+        alert("⛔️ Usuario o clave invalida")
+    }
 }
